e7537deb8a8af511d7b015e7db1913703df4496d
Andrey Shaat <104313094+ASHGOLDOFFICIAL@users.noreply.github.com>
2024-12-02 16:14:35 +0500

    Убрал методы equals и hashCode у Ticket.

--- src/main/java/ru/naumen/collection/task1/Ticket.java
+++ src/main/java/ru/naumen/collection/task1/Ticket.java
@@ -1,7 +1,5 @@
 package ru.naumen.collection.task1;
 
-import java.util.Objects;
-
 /**
  * Билет
  *
@@ -12,25 +10,7 @@ public class Ticket {
     private long id;
     private String client;
 
-    public Ticket(long id, String client) {
-        this.id = id;
-        this.client = client;
-    }
-
     public long getId() {
         return id;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == this) return true;
-        if (obj == null || obj.getClass() != this.getClass()) return false;
-        var that = (Ticket) obj;
-        return this.id == that.id;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
     }
 }
